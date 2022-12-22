a16471638cda9951dcc764edb080307166eb8776
davidecardelli <davide.cardelli.94@gmail.com>
2022-12-22 10:17:15 +0100

    milesdtone 0

---
+++ script.js
@@ -0,0 +1,29 @@
+// ---------- TRACCIA ----------
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+
+// BONUS 2:
+// Organizzare i singoli membri in card/schede e rendere la pagina gradevole (potete usare lo screen in allegato come spunto
+
+const member = [
+    {name: 'Wayne Barnett', role: 'Founder & CEO', img: 'wayne-barnett-founder.ceo.jpg'},
+    {name: 'Angela Caroll',role: 'Chief Editor',img: 'angela-caroll-chief-editor.jpg'},
+    {name: 'Walter Gordon',role: 'Office Manager',img: 'walter-gordon-office-manager.jpg'},
+    {name: 'Angela Lopez',role: 'Social Media Manager',img: 'angela-lopez-social-media-manager-jpg'},
+    {name: 'Scott Estrada',role: 'Developer',img: 'scott-estrada-developer.jpg'},
+    {name: 'Barbara Ramos',role: 'Graphic Designer',img: 'barbara-ramos-graphic-designer.jpg'}
+]
+
+console.log(member);
